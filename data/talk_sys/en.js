c383542d5539ea95482bb49ef5b1cb86908ffc2b
nzws (yuzulabo) <git@nzws.me>
2018-07-15 21:37:53 +0900

    fix text thx: @imbsky

--- data/talk_sys/en.js
+++ data/talk_sys/en.js
@@ -1,6 +1,6 @@
 exports.lang = [
-    'Only one user\'s request can be processed at one time!',
-    'Only one image can be processed at one time!',
+    'Only one user\'s request can process at one time!',
+    'Only one image can process at one time!',
     'Please use the image, I can\'t process except image...',
     'To use the image effect, you need to send an image or the user ID of the icon what you want to use! \n Image effect documentation: Coming soon.',
     'To use image effects, you need to specify the effect mode! \n Image effect documentation: Coming soon.',
